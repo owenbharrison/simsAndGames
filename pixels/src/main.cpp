5a40a1ca1e715114ac2918e5f091d6ed871bad64
Owen Harrison <owenbharrison@gmail.com>
2024-12-28 12:39:18 -0700

    Adding pixels sim

---
+++ pixels/src/main.cpp
@@ -0,0 +1,547 @@
+/*todo:
+verlet integration?
+forces
+mass
+center of mass
+torques
+moment of inertia
+rot_acc
+*/
+
+#include "common/myPixelGameEngine.h"
+using olc::vf2d;
+using olc::vi2d;
+
+#include "aabb.h"
+#include "pixel_set.h"
+
+std::ostream& operator<<(std::ostream& o, const PixelSet& ps) {
+	for(int j=0; j<ps.getH(); j++) {
+		for(int i=0; i<ps.getW(); i++) {
+			//dont print empty
+			const auto& p=ps(i, j);
+			if(p!=PixelSet::Empty) o<<int(p);
+			else o<<' ';
+
+			o<<' ';
+		}
+		o<<'\n';
+	}
+	return o;
+}
+
+//clever default param placement:
+//random()=0-1
+//random(a)=0-a
+//random(a, b)=a-b
+float random(float b=1, float a=0) {
+	static const float rand_max=RAND_MAX;
+	float t=rand()/rand_max;
+	return a+t*(b-a);
+}
+
+constexpr float Pi=3.1415927f;
+
+//thanks wikipedia + pattern recognition
+vf2d lineLineIntersection(
+	const vf2d& a, const vf2d& b,
+	const vf2d& c, const vf2d& d) {
+	vf2d ab=a-b, ac=a-c, cd=c-d;
+	return vf2d(
+		ac.cross(cd),
+		ac.cross(ab)
+	)/ab.cross(cd);
+}
+
+PixelSet pixelsetFromOutline(const std::vector<vf2d>& outline) {
+	//get bounds of outline
+	AABB box;
+	for(const auto& o:outline) box.fitToEnclose(o);
+
+	//determine spacing
+	vf2d size=box.max-box.min;
+	const float scale=5;
+	int w=1+size.x/scale, h=1+size.y/scale;
+
+	//for every point
+	PixelSet p(w, h);
+	p.pos=box.min;
+	p.scale=scale;
+	for(int i=0; i<w; i++) {
+		for(int j=0; j<h; j++) {
+			//is it inside the outline
+			vf2d pos=p.localToWorld(i, j);
+
+			//deterministic, but less artifacting
+			float angle=random(2*Pi);
+			vf2d dir(std::cosf(angle), std::sinf(angle));
+
+			//polygon raycast algorithm
+			int num=0;
+			for(int k=0; k<outline.size(); k++) {
+				const auto& a=outline[k];
+				const auto& b=outline[(k+1)%outline.size()];
+				vf2d tu=lineLineIntersection(a, b, pos, pos+dir);
+				if(tu.x>0&&tu.x<1&&tu.y>0) num++;
+			}
+
+			//odd? inside!
+			p(i, j)=num%2;
+		}
+	}
+
+	//should this be encapsulated into a function?
+	p.compress();
+	p.updateTypes();
+	p.updateMeshes();
+
+	return p;
+}
+
+struct PixelGame : MyPixelGameEngine {
+	PixelGame() {
+		sAppName="Testing";
+	}
+
+	vf2d mouse_pos;
+	vf2d prev_mouse_pos;
+	vf2d gravity;
+
+	std::list<PixelSet*> pixelsets;
+
+	bool show_bounds=false;
+	bool show_wireframes=false;
+	bool show_grids=false;
+
+	vf2d drag_start;
+	PixelSet* drag_set=nullptr;
+
+	std::vector<vf2d> addition;
+	float addition_timer=0;
+
+	void placeRandom() {
+		for(const auto& p:pixelsets) {
+			p->rot=random(2*Pi);
+			p->updateRot();
+
+			//aabb needs cossin
+			AABB box=p->getAABB();
+			p->pos.x+=random(-box.min.x, ScreenWidth()-box.max.x);
+			p->pos.y+=random(-box.min.y, ScreenHeight()-box.max.y);
+		}
+	}
+
+	bool on_init() override {
+		srand(time(0));
+
+		gravity={0, 0};
+
+#pragma region CONSTRUCTION
+		{//make floor
+			PixelSet* thing=new PixelSet(12, 3);
+			for(int i=0; i<thing->getW()*thing->getH(); i++) {
+				thing->grid[i]=PixelSet::Normal;
+			}
+			thing->updateTypes();
+			thing->updateMeshes();
+
+			thing->scale=20;
+
+			pixelsets.push_back(thing);
+		}
+
+		{//make ball
+			const int sz=16;
+			PixelSet* thing=new PixelSet(sz, sz);
+			float r=.5f*sz, r_sq=r*r;
+			for(int x=0; x<sz; x++) {
+				float dx=x-.5f*sz;
+				for(int y=0; y<sz; y++) {
+					float dy=y-.5f*sz;
+					(*thing)(x, y)=dx*dx+dy*dy<r_sq;
+				}
+			}
+			thing->compress();
+			thing->updateTypes();
+			thing->updateMeshes();
+
+			thing->scale=8;
+
+			pixelsets.push_back(thing);
+		}
+
+		{//make triangle
+			const int w=56, h=44;
+
+			//rasterize it
+			olc::Sprite tri(w, h);
+			SetDrawTarget(&tri);
+			Clear(olc::BLACK);
+			FillTriangle(w/2, 0, w-1, h-1, 0, h-1, olc::WHITE);
+			SetDrawTarget(nullptr);
+
+			//copy over data
+			PixelSet* thing=new PixelSet(w, h);
+			for(int i=0; i<w; i++) {
+				for(int j=0; j<h; j++) {
+					(*thing)(i, j)=tri.GetPixel(i, j).r>0;
+				}
+			}
+			thing->compress();
+			thing->updateTypes();
+			thing->updateMeshes();
+
+			thing->scale=5;
+
+			pixelsets.push_back(thing);
+		}
+#pragma endregion
+
+		placeRandom();
+
+		ConsoleCaptureStdOut(true);
+
+		return true;
+	}
+
+	void reset() {
+		for(const auto& p:pixelsets) delete p;
+		pixelsets.clear();
+	}
+
+	bool on_exit() override {
+		reset();
+
+		return true;
+	}
+
+	bool on_update(float dt) override {
+		prev_mouse_pos=mouse_pos;
+		mouse_pos=GetMousePos();
+
+#pragma region USER INPUT
+		if(!IsConsoleShowing()) {
+			//mouse grabbing
+			const auto left_mouse=GetMouse(olc::Mouse::LEFT);
+			if(left_mouse.bPressed) {
+				drag_start=mouse_pos;
+				//which dragset is the mouse on?
+				drag_set=nullptr;
+				for(const auto& p:pixelsets) {
+					//is mouse in bounds?
+					if(!p->getAABB().contains(mouse_pos)) continue;
+
+					//are local coords valid?
+					vf2d ij=p->worldToLocal(mouse_pos);
+					int i=ij.x, j=ij.y;
+					if(!p->inRangeX(i)||!p->inRangeY(j)) continue;
+
+					//is the block solid?
+					if((*p)(i, j)!=PixelSet::Empty) {
+						drag_set=p;
+						break;
+					}
+				}
+			}
+			if(left_mouse.bHeld) {
+				//update dragset pos
+				if(drag_set) drag_set->pos+=mouse_pos-drag_start;
+				drag_start=mouse_pos;
+			}
+			if(left_mouse.bReleased) {
+				//let go of that sucker
+				drag_set=nullptr;
+			}
+
+			//add objects
+			const auto a_key=GetKey(olc::Key::A);
+			if(a_key.bPressed) addition.clear();
+			//every now and then...
+			if(addition_timer<0) {
+				addition_timer=.05f;
+
+				if(a_key.bHeld) {
+					//dont put points too close
+					bool exist=false;
+					for(const auto& a:addition) {
+						if((a-mouse_pos).mag()<1) {
+							exist=true;
+							break;
+						}
+					}
+					//add point to addition
+					if(!exist) addition.emplace_back(mouse_pos);
+				}
+			}
+			addition_timer-=dt;
+			if(a_key.bReleased) {
+				if(addition.size()>=3) {
+					PixelSet* thing=new PixelSet(pixelsetFromOutline(addition));
+
+					pixelsets.emplace_back(thing);
+
+					std::cout<<"added "<<thing->getW()<<'x'<<thing->getH()<<" pixelset\n";
+				}
+				addition.clear();
+			}
+
+			//slice objects
+			//THIS DOESNT ALWAYS WORK
+			//implement bresenhams
+			//clip segment against local space
+			if(GetKey(olc::Key::S).bHeld) {
+				AABB seg_box;
+				seg_box.fitToEnclose(prev_mouse_pos);
+				seg_box.fitToEnclose(mouse_pos);
+
+				//check every pixelset
+				for(const auto& p:pixelsets) {
+
+					//is mouse in bounds?
+					if(!seg_box.overlaps(p->getAABB())) continue;
+
+					//convert segment to pixelset space
+					vf2d s0=p->worldToLocal(prev_mouse_pos);
+					vf2d s1=p->worldToLocal(mouse_pos);
+
+					//check all blocks against it
+					bool edited=false;
+					for(int i=0; i<p->getW(); i++) {
+						for(int j=0; j<p->getH(); j++) {
+							//skip air
+							if((*p)(i, j)==PixelSet::Empty) continue;
+
+							//find corners of box
+							vf2d v0(i, j), v3(1+i, 1+j);
+							vf2d v1(1+i, j), v2(i, 1+j);
+
+							//check each edge
+							bool hit=false;
+							for(int p=0; p<4; p++) {
+								vf2d a, b;
+								switch(p) {
+									case 0: a=v0, b=v1; break;
+									case 1: a=v1, b=v2; break;
+									case 2: a=v2, b=v3; break;
+									case 3: a=v3, b=v0; break;
+								}
+								vf2d tu=lineLineIntersection(a, b, s0, s1);
+								if(tu.x>0&&tu.x<1&&tu.y>0&&tu.y<1) {
+									hit=true;
+									break;
+								}
+							}
+							if(hit) {
+								(*p)(i, j)=PixelSet::Empty;
+								edited=true;
+							}
+						}
+					}
+					if(edited) {
+						p->compress();
+						p->updateTypes();
+						p->updateMeshes();
+						std::cout<<"sliced pixelset\n";
+					}
+				}
+			}
+
+			//remove objects
+			if(GetKey(olc::Key::DEL).bPressed) {
+				for(auto it=pixelsets.begin(); it!=pixelsets.end();) {
+					
+					//is mouse in bounds?
+					const auto& p=*it;
+					if(p->getAABB().contains(mouse_pos)) {
+					
+						//are local coords valid?
+						vf2d ij=p->worldToLocal(mouse_pos);
+						int i=ij.x, j=ij.y;
+						if(p->inRangeX(i)&&p->inRangeY(j)) {
+							
+							//is the block solid?
+							if((*p)(i, j)!=PixelSet::Empty) {
+								
+								//deallocate and remove
+								delete p;
+								it=pixelsets.erase(it);
+								std::cout<<"removed pixelset\n";
+
+								continue;
+							}
+						}
+					}
+
+					it++;
+				}
+			}
+
+			//randomize rotations
+			if(GetKey(olc::Key::R).bPressed) placeRandom();
+
+			//visual toggles with xor
+			if(GetKey(olc::Key::B).bPressed) show_bounds^=true;
+			if(GetKey(olc::Key::W).bPressed) show_wireframes^=true;
+			if(GetKey(olc::Key::G).bPressed) show_grids^=true;
+		}
+
+		if(GetKey(olc::Key::ESCAPE).bPressed) ConsoleShow(olc::Key::ESCAPE);
+#pragma endregion
+
+#pragma region PHYSICS
+		//dynamics
+		for(const auto& p:pixelsets) {
+			p->applyForce(gravity);
+			p->update(dt);
+		}
+
+		//clear collide display
+		for(const auto& p:pixelsets) {
+			memset(p->colliding, false, sizeof(bool)*p->getW()*p->getH());
+		}
+
+		//for every pixelset
+		for(const auto& p:pixelsets) {
+			//check every other
+			for(const auto& o:pixelsets) {
+				//dont check self
+				if(o==p) continue;
+
+				p->collide(*o);
+			}
+		}
+#pragma endregion
+
+		return true;
+	}
+
+	bool OnConsoleCommand(const std::string& line) override {
+		std::stringstream line_str(line);
+		std::string cmd; line_str>>cmd;
+
+		if(cmd=="clear") {
+			ConsoleClear();
+		}
+
+		if(cmd=="reset") {
+			std::cout<<"removed "<<pixelsets.size()<<" pixelsets\n";
+			reset();
+		}
+
+		if(cmd=="count") {
+			int num=pixelsets.size();
+			std::cout<<"there ";
+			std::cout<<(num==1?"is ":"are ");
+			std::cout<<num;
+			std::cout<<" pixelset";
+			if(num!=1) std::cout<<'s';
+			std::cout<<'\n';
+		}
+
+		return true;
+	}
+
+	bool on_render() override {
+		//draw grey background
+		Clear(olc::GREY);
+		SetDecalMode(show_wireframes?olc::DecalMode::WIREFRAME:olc::DecalMode::NORMAL);
+
+		//show addition
+		if(addition.size()) {
+			vf2d prev;
+			for(int i=0; i<addition.size(); i++) {
+				const auto& a=addition[i];
+				const auto& b=addition[(i+1)%addition.size()];
+				DrawThickLine(a, b, 1, olc::CYAN);
+			}
+		}
+
+		//show all pixelsets
+		for(const auto& p:pixelsets) {
+			//draw rectangle bounding box
+			if(show_bounds) {
+				//color if it overlaps
+				AABB box=p->getAABB();
+				olc::Pixel col=olc::GREEN;
+				for(const auto& o:pixelsets) {
+					//dont check self
+					if(o==p) continue;
+
+					if(box.overlaps(o->getAABB())) {
+						col=olc::RED;
+						break;
+					}
+				}
+
+				vf2d v1(box.max.x, box.min.y);
+				vf2d v2(box.min.x, box.max.y);
+				DrawThickLine(box.min, v1, 1, col);
+				DrawThickLine(v1, box.max, 1, col);
+				DrawThickLine(box.max, v2, 1, col);
+				DrawThickLine(v2, box.min, 1, col);
+			}
+
+			//show local grid
+			if(show_grids) {
+				//draw "vertical" ticks
+				for(int i=0; i<=p->getW(); i++) {
+					DrawLineDecal(p->localToWorld(i, 0), p->localToWorld(i, p->getH()), olc::DARK_GREY);
+				}
+
+				//draw "horizontal" ticks
+				for(int j=0; j<=p->getH(); j++) {
+					DrawLineDecal(p->localToWorld(0, j), p->localToWorld(p->getW(), j), olc::DARK_GREY);
+				}
+
+				//draw axes
+				DrawThickLine(p->localToWorld(-1, 0), p->localToWorld(p->getW()+1, 0), 1, olc::BLACK);
+				DrawThickLine(p->localToWorld(0, -1), p->localToWorld(0, p->getH()+1), 1, olc::BLACK);
+			}
+
+			//render meshes
+			for(const auto& m:p->meshes) {
+				vf2d pos=p->localToWorld(m.i, m.j);
+				vf2d size(p->scale*m.w, p->scale*m.h);
+				DrawRotatedDecal(pos, getRectDecal(), p->rot, {0, 0}, size, m.col);
+			}
+
+#pragma region EXPERIMENTAL
+			//render colliding pixels
+			for(int i=0; i<p->getW(); i++) {
+				for(int j=0; j<p->getH(); j++) {
+					if(!p->colliding[p->ix(i, j)]) continue;
+
+					vf2d pos=p->localToWorld(i, j);
+					vf2d size(p->scale, p->scale);
+					DrawRotatedDecal(pos, getRectDecal(), p->rot, {0, 0}, size, olc::RED);
+				}
+			}
+
+			//show hover block
+			//is mouse in bounds?
+			if(p->getAABB().contains(mouse_pos)) {
+				//are local coords valid?
+				vf2d ij=p->worldToLocal(mouse_pos);
+				int i=ij.x, j=ij.y;
+				if(p->inRangeX(i)&&p->inRangeY(j)) {
+					//is the block solid?
+					if((*p)(i, j)!=PixelSet::Empty) {
+						vf2d pos=p->localToWorld(i, j);
+						vf2d size(p->scale, p->scale);
+						DrawRotatedDecal(pos, getRectDecal(), p->rot, {0, 0}, size, olc::GREEN);
+					}
+				}
+			}
+#pragma endregion
+		}
+
+		return true;
+	}
+};
+
+int main() {
+	PixelGame t;
+	if(t.Construct(800, 640, 1, 1, false, true)) t.Start();
+
+	return 0;
+}
---
+++ pixels/src/pixel_set.h
@@ -0,0 +1,336 @@
+/*TODO
+impl floodfill
+* compress shape into multiples
+* pass func to determine what to do with new ones
+impl polygon to PixelSet
+*/
+
+#pragma once
+#ifndef PIXELSET_CLASS_H
+#define PIXELSET_CLASS_H
+
+struct Mesh {
+	int i=0, j=0, w=0, h=0;
+	olc::Pixel col;
+
+	Mesh() {}
+
+	Mesh(int i_, int j_, int w_, int h_, olc::Pixel c) : i(i_), j(j_), w(w_), h(h_), col(c) {}
+};
+
+typedef unsigned char byte;
+
+#include "aabb.h"
+
+class PixelSet {
+	int w, h;
+
+	void copyFrom(const PixelSet& p), clear();
+
+public:
+	byte* grid=nullptr;
+	bool* colliding=nullptr;
+
+	enum {
+		Empty,
+		Normal,
+		Edge
+	};
+
+	vf2d pos, vel, acc;
+	float rot=0;
+	//this is causing annoying issues...
+	vf2d cossin;
+	float scale=1;
+
+	std::vector<Mesh> meshes;
+
+	PixelSet() : PixelSet(1, 1) {}
+
+	PixelSet(int _w, int _h) : w(_w), h(_h) {
+		grid=new byte[w*h];
+		colliding=new bool[w*h];
+		updateRot();
+	}
+
+	//1
+	PixelSet(const PixelSet& p) {
+		copyFrom(p);
+	}
+
+	//2
+	~PixelSet() {
+		clear();
+	}
+
+	//3
+	PixelSet& operator=(const PixelSet& p) {
+		if(&p==this) return *this;
+
+		clear();
+
+		copyFrom(p);
+
+		return *this;
+	}
+
+	//getters
+	bool inRangeX(int i) const { return i>=0&&i<w; }
+	bool inRangeY(int j) const { return j>=0&&j<h; }
+
+	int ix(int i, int j) const { return i+w*j; }
+
+	int getW() const { return w; }
+	int getH() const { return h; }
+
+	const byte& operator()(int i, int j) const { return grid[ix(i, j)]; }
+	byte& operator()(int i, int j) { return grid[ix(i, j)]; }
+
+	bool empty() const {
+		for(int i=0; i<w*h; i++) {
+			if(grid[i]!=Empty) return false;
+		}
+		return true;
+	}
+
+	//helpers?
+	AABB getAABB() const {
+		AABB box;
+
+		//for each corner point
+		for(int p=0; p<4; p++) {
+			int i=0, j=0;
+			switch(p) {
+				case 1: i=w; break;
+				case 2: i=w, j=h; break;
+				case 3: j=h; break;
+			}
+
+			//rotate point and fit in box
+			box.fitToEnclose(localToWorld(i, j));
+		}
+		return box;
+	}
+
+	vf2d localToWorld(int i, int j) const {
+		//order of rotate then scale doesnt matter.
+		vf2d scaled(scale*i, scale*j);
+		vf2d rotated(
+		cossin.x*scaled.x-cossin.y*scaled.y,
+		cossin.y*scaled.x+cossin.x*scaled.y
+		);
+		//translate
+		return pos+rotated;
+	}
+
+	//inverse of rotation matrix is its transpose!
+	vf2d worldToLocal(const vf2d& p) {
+		//undo transformations...
+		vf2d rotated=p-pos;
+		vf2d scaled(
+		cossin.x*rotated.x+cossin.y*rotated.y,
+		cossin.x*rotated.y-cossin.y*rotated.x
+		);
+		return scaled/scale;
+	}
+
+	void applyForce(const vf2d& f) {
+		acc+=f;
+	}
+
+	void updateRot() {
+		//precompute trig
+		cossin={std::cosf(rot), std::sinf(rot)};
+	}
+
+	void update(float dt) {
+		//euler explicit?
+		vel+=acc*dt;
+		pos+=vel*dt;
+
+		//reset forces
+		acc={0, 0};
+
+		updateRot();
+	}
+
+	//think of this as a trim whitespace function
+	void compress() {
+		//uhh
+		if(empty()) return;
+
+		//find bounds of solids:
+		int i_min=w-1, j_min=h-1;
+		int i_max=0, j_max=0;
+
+		//for every solid block
+		for(int i=0; i<w; i++) {
+			for(int j=0; j<h; j++) {
+				if(grid[ix(i, j)]==Empty) continue;
+
+				//update bounds
+				if(i<i_min) i_min=i;
+				if(j<j_min) j_min=j;
+				if(i>i_max) i_max=i;
+				if(j>j_max) j_max=j;
+			}
+		}
+
+		//is compression warranted?
+		if(i_min!=0||j_min!=0||i_max!=w-1||j_max!=h-1) {
+			//copy to smaller grid
+			PixelSet p(1+i_max-i_min, 1+j_max-j_min);
+			p.pos=pos;
+			p.rot=rot;
+			p.cossin=cossin;
+			p.scale=scale;
+			for(int i=0; i<p.getW(); i++) {
+				for(int j=0; j<p.getH(); j++) {
+					p(i, j)=grid[ix(i_min+i, j_min+j)];
+				}
+			}
+
+			//update self
+			operator=(p);
+
+			//retranslate based on minimum
+			pos=localToWorld(i_min, j_min);
+		}
+	}
+
+	//edge detection for collision routine
+	void updateTypes() {
+		for(int i=0; i<w; i++) {
+			for(int j=0; j<h; j++) {
+				auto& t=grid[ix(i, j)];
+				if(t==Empty) continue;
+
+				//which neighbors are empty?
+				bool left=i==0||grid[ix(i-1, j)]==Empty;
+				bool right=i==w-1||grid[ix(i+1, j)]==Empty;
+				bool up=j==0||grid[ix(i, j-1)]==Empty;
+				bool down=j==h-1||grid[ix(i, j+1)]==Empty;
+				//if any neighbor empty, make edge else normal
+				t=left||right||up||down?Edge:Normal;
+			}
+		}
+	}
+
+	//check MY edges against THEIR everything
+	void collide(PixelSet& p) {
+		//check bounding boxes
+		if(!getAABB().overlaps(p.getAABB())) return;
+
+		//for each of MY blocks
+		for(int i=0; i<w; i++) {
+			for(int j=0; j<h; j++) {
+				//skip if not edge
+				if(grid[ix(i, j)]!=PixelSet::Edge) continue;
+
+				//my block in world space
+				vf2d my_world=localToWorld(i, j);
+
+				//my block in their space
+				vf2d my_in_their=p.worldToLocal(my_world);
+
+				//is it a valid position?
+				int pi=my_in_their.x, pj=my_in_their.y;
+				if(!p.inRangeX(pi)||!p.inRangeY(pj)) continue;
+
+				//is there anything there?
+				if(p(pi, pj)==Empty) continue;
+
+				//update flags
+				p.colliding[p.ix(pi, pj)]=true;
+				colliding[ix(i, j)]=true;
+			}
+		}
+	}
+
+	void clearMeshes() {
+		meshes.clear();
+	}
+
+	void updateMeshes() {
+		clearMeshes();
+
+		bool* meshed=new bool[w*h];
+		memset(meshed, false, sizeof(bool)*w*h);
+		for(int i=0; i<w; i++) {
+			for(int j=0; j<h; j++) {
+				//dont mesh air
+				const auto& curr=grid[ix(i, j)];
+				if(curr==PixelSet::Empty) continue;
+
+				//dont remesh
+				if(meshed[ix(i, j)]) continue;
+
+				//greedy meshing!
+
+				//combine rows
+				int ext_w=1;
+				for(int i_=1+i; i_<w; i_++, ext_w++) {
+					//only mesh similar and dont remesh
+					if(grid[ix(i_, j)]!=curr||meshed[ix(i_, j)]) break;
+				}
+
+				//combine entire columns
+				int ext_h=1;
+				for(int j_=1+j; j_<h; j_++, ext_h++) {
+					bool able=true;
+					for(int i_=0; i_<ext_w; i_++) {
+						//only mesh similar and dont remesh
+						if(grid[ix(i+i_, j_)]!=curr||meshed[ix(i+i_, j_)]) {
+							able=false;
+							break;
+						}
+					}
+					if(!able) break;
+				}
+
+				//set meshed
+				for(int i_=0; i_<ext_w; i_++) {
+					for(int j_=0; j_<ext_h; j_++) {
+						meshed[ix(i+i_, j+j_)]=true;
+					}
+				}
+
+				//triangulate
+				olc::Pixel col;
+				switch(curr) {
+					case PixelSet::Normal: col=olc::WHITE; break;
+					case PixelSet::Edge: col=olc::BLUE; break;
+				}
+				meshes.emplace_back(i, j, ext_w, ext_h, col);
+			}
+		}
+		delete[] meshed;
+	}
+};
+
+void PixelSet::copyFrom(const PixelSet& p) {
+	//reallocate and copy over grid stuff
+	w=p.w, h=p.h;
+	grid=new byte[w*h];
+	colliding=new bool[w*h];
+	memcpy(grid, p.grid, sizeof(byte)*w*h);
+	//copying of meshed not necessary
+
+	//not sure if i need ALL of these, but whetever
+	pos=p.pos;
+	vel=p.vel;
+	acc=p.acc;
+
+	rot=p.rot;
+	cossin=p.cossin;
+
+	scale=p.scale;
+}
+
+void PixelSet::clear() {
+	delete[] grid;
+	delete[] colliding;
+
+	clearMeshes();
+}
+#endif//PIXELSET_CLASS_H
