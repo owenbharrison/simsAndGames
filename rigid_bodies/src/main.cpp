9c47928e0c2fa0d0e85b1a16f93416be92152e69
Owen Harrison <owenbharrison@gmail.com>
2025-01-18 12:42:41 -0700

    added COM, MOI, mass view, object addition and removal

--- rigid_bodies/src/main.cpp
+++ rigid_bodies/src/main.cpp
@@ -9,13 +9,22 @@ struct RigidBodyDemo : olc::PixelGameEngine {
 		sAppName="Rigid Body Simulation";
 	}
 
+	//main shape list
 	std::list<Shape*> shapes;
 
+	//user input stuff
 	vf2d mouse_pos;
 
+	std::list<vf2d> addition;
+	float addition_timer=0;
+
+	//graphics toggles
 	bool show_bounds=false;
+	bool show_masses=false;
 
 	bool OnUserCreate() override {
+		srand(time(0));
+
 		shapes.emplace_back(new Shape(.5f*vf2d(ScreenWidth(), ScreenHeight()), 50, 12));
 
 		return true;
@@ -36,18 +45,100 @@ struct RigidBodyDemo : olc::PixelGameEngine {
 #pragma region USER_INPUT
 		mouse_pos=GetMousePos();
 
+		//graphics toggles
 		if(GetKey(olc::Key::B).bPressed) show_bounds^=true;
+		if(GetKey(olc::Key::M).bPressed) show_masses^=true;
+
+		//add objects
+		const auto a_key=GetKey(olc::Key::A);
+		if(a_key.bPressed) {
+			addition.clear();
+		}
+		//every now and then
+		if(addition_timer<0) {
+			addition_timer=.05f;
+
+			if(a_key.bHeld) {
+				//make sure not too close
+				bool exists=false;
+				for(const auto& a:addition) {
+					if((a-mouse_pos).mag()<5) {
+						exists=true;
+						break;
+					}
+				}
+				//add mouse point
+				if(!exists) addition.emplace_back(mouse_pos);
+			}
+		}
+		addition_timer-=dt;
+		if(a_key.bReleased) {
+			//add shape IF at least a triangle.
+			if(addition.size()>2) shapes.emplace_back(new Shape(addition));
+			addition.clear();
+		}
+
+		//remove objects
+		if(GetKey(olc::Key::X).bHeld) {
+			for(auto it=shapes.begin(); it!=shapes.end();) {
+				const auto& s=*it;
+				if(s->contains(mouse_pos)) {
+					delete s;
+					it=shapes.erase(it);
+				} else it++;
+			}
+		}
 #pragma endregion
 
 #pragma region PHYSICS
+		//seperating axis theorem
+		{
+			//for every shape pair
+			for(auto ait=shapes.begin(); ait!=shapes.end(); ait++) {
+				for(auto bit=std::next(ait); bit!=shapes.end(); bit++) {
+					//get combined list of axes
+					std::list<vf2d> axes;
+					for(int i=0; i<2; i++) {
+						const auto& s=*(i?ait:bit);
+						//find tangents of shape's edges
+						for(int j=0; j<s->getNum(); j++) {
+							const auto& a=s->localToWorld(s->points[j]);
+							const auto& b=s->localToWorld(s->points[(j+1)%s->getNum()]);
+							axes.emplace_back((a-b).norm());
+						}
+					}
+
+					float a_min=INFINITY, a_max=-a_min;
+					float b_min=a_min, b_max=a_max;
+					//project all points on to each axis
+					for(const auto& x:axes) {
+
+					}
+				}
+			}
+
+		}
+
 		for(const auto& s:shapes) {
 			//s->applyForce(s->mass*gravity, s->pos);
-			//s->update(dt);
+			s->update(dt);
 		}
 #pragma endregion
 
 #pragma region RENDER
 		Clear(olc::GREY);
+
+		//show addition
+		if(addition.size()) {
+			bool is_first=true;
+			vf2d first, prev;
+			for(const auto& a:addition) {
+				if(is_first) first=a, is_first=false;
+				else DrawLineDecal(prev, a, olc::BLUE);
+				prev=a;
+			}
+			DrawLineDecal(first, prev, olc::BLUE);
+		}
 
 		//show all shapes
 		for(const auto& s:shapes) {
@@ -122,6 +213,11 @@ struct RigidBodyDemo : olc::PixelGameEngine {
 					s->localToWorld(b),
 					olc::BLACK
 				);
+			}
+
+			if(show_masses) {
+				DrawLineDecal(vf2d(s->pos.x-15, s->pos.y), vf2d(s->pos.x+15, s->pos.y), olc::BLACK);
+				DrawLineDecal(vf2d(s->pos.x, s->pos.y-15), vf2d(s->pos.x, s->pos.y+15), olc::BLACK);
 			}
 		}
 #pragma endregion
--- rigid_bodies/src/shape.h
+++ rigid_bodies/src/shape.h
@@ -10,27 +10,45 @@ vf2d polar(float rad, float angle) {
 
 #include "aabb.h"
 
+//clever default param placement:
+//random()=0-1
+//random(a)=0-a
+//random(a, b)=a-b
+float random(float b=1, float a=0) {
+	static const float rand_max=RAND_MAX;
+	float t=rand()/rand_max;
+	return a+t*(b-a);
+}
+
+//thanks wikipedia + pattern recognition
+//this returns (t, u), NOT the point
+vf2d lineLineIntersection(
+	const vf2d& a, const vf2d& b,
+	const vf2d& c, const vf2d& d) {
+	vf2d ab=a-b, ac=a-c, cd=c-d;
+	return vf2d(
+		ac.cross(cd),
+		ac.cross(ab)
+	)/ab.cross(cd);
+}
+
 class Shape {
 	int num_pts;
 
 	void copyFrom(const Shape&), clear();
-	void ensureWinding();
 
 public:
-
 	vf2d* points=nullptr;
-	vf2d pos, old_pos;
-	
-	float rot=0, old_rot=0;
-	vf2d cossin{1, 0};
-
+	vf2d pos, old_pos, forces;
 	float mass=1;
+	
+	float rot=0, old_rot=0, torques=0;
+	vf2d cossin{1, 0};
+	float moment_of_inertia=1;
 
 	Shape() : Shape({0, 0}, 0, 1) {}
 
 	Shape(vf2d ctr, float rad, int n) {
-		pos=ctr;
-
 		//allocate
 		num_pts=n;
 		points=new vf2d[num_pts];
@@ -38,11 +56,26 @@ public:
 		//make n-gon
 		for(int i=0; i<num_pts; i++) {
 			float angle=2*Pi*i/num_pts;
-			points[i]=polar(rad, angle);
+			points[i]=ctr+polar(rad, angle);
 		}
 
-		ensureWinding();
-		mass=getArea();
+		initMass();
+		initInertia();
+	}
+
+	Shape(const std::list<vf2d>& pts) {
+		//allocate
+		num_pts=pts.size();
+		points=new vf2d[num_pts];
+
+		//copy over
+		int i=0;
+		for(const auto& p:pts) {
+			points[i++]=p;
+		}
+
+		initMass();
+		initInertia();
 	}
 
 	Shape(const Shape& s) {
@@ -70,17 +103,6 @@ public:
 		return num_pts;
 	}
 
-	float getArea() const {
-		float sum=0;
-		for(int i=0; i<num_pts; i++) {
-			const auto& a=points[i];
-			const auto& b=points[(i+1)%num_pts];
-			sum+=a.cross(b);
-		}
-		//parallelograms/2=triangles
-		return sum/2;
-	}
-
 	//optimize??
 	AABB getAABB() const {
 		AABB box;
@@ -88,6 +110,26 @@ public:
 			box.fitToEnclose(localToWorld(points[i]));
 		}
 		return box;
+	}
+
+	//polygon raycasting algorithm
+	bool contains(const vf2d& pt) {
+		if(!getAABB().contains(pt)) return false;
+
+		//deterministic, but no artifacts
+		vf2d pa=worldToLocal(pt);
+		vf2d pb=pa+polar(1, random(2*Pi));
+
+		int num_ix=0;
+		for(int i=0; i<num_pts; i++) {
+			const auto& a=points[i];
+			const auto& b=points[(i+1)%num_pts];
+			vf2d tu=lineLineIntersection(a, b, pa, pb);
+			if(tu.x>0&&tu.x<1&&tu.y>0) num_ix++;
+		}
+
+		//odd? inside!
+		return num_ix%2;
 	}
 
 	vf2d localToWorld(const vf2d& p) const {
@@ -110,6 +152,96 @@ public:
 		);
 		return original;
 	}
+
+#pragma region PHYSICS
+	void initMass() {
+		//signed area calculation
+		mass=0;
+		for(int i=0; i<num_pts; i++) {
+			const auto& a=points[i];
+			const auto& b=points[(i+1)%num_pts];
+			mass+=a.x*b.y-b.x*a.y;
+		}
+		//parallelogram/2=triangle
+		mass/=2;
+
+		//ensure points wound consistently
+		if(mass<0) {
+			std::reverse(points, points+num_pts);
+			mass=-mass;
+		}
+
+		//COM of polygon
+		vf2d center_of_mass;
+		for(int i=0; i<num_pts; i++) {
+			const auto& a=points[i];
+			const auto& b=points[(i+1)%num_pts];
+			float cross=a.x*b.y-b.x*a.y;
+			center_of_mass+=cross*(a+b);
+		}
+		center_of_mass/=6*mass;
+
+		//recenter points about COM=pos
+		for(int i=0; i<num_pts; i++) {
+			points[i]-=center_of_mass;
+		}
+		pos=center_of_mass;
+		old_pos=pos;
+	}
+
+	//MOI of polygon where com=origin
+	void initInertia() {
+		moment_of_inertia=0;
+		for(int i=0; i<num_pts; i++) {
+			const auto& a=points[i];
+			const auto& b=points[(i+1)%num_pts];
+			float cross=a.x*b.y-b.x*a.y;
+			float x_comp=a.x*a.x+a.x*b.x+b.x*b.x;
+			float y_comp=a.y*a.y+a.y*b.y+b.y*b.y;
+			moment_of_inertia+=cross*(x_comp+y_comp);
+		}
+		moment_of_inertia/=12;
+	}
+
+	void applyForce(const vf2d& f, const vf2d& p) {
+		//update translational forces
+		forces+=f;
+
+		//torque based on pivot dist
+		vf2d r=p-pos;
+		torques+=r.cross(f);
+	}
+
+	//percompute for rotation matrix
+	void updateRot() {
+		cossin=polar(1, rot);
+	}
+
+	void update(float dt) {
+	//store vel and update oldpos
+		vf2d vel=pos-old_pos;
+		old_pos=pos;
+
+		//verlet integration
+		vf2d acc=forces/mass;
+		pos+=vel+acc*dt*dt;
+
+		//reset forces
+		forces={0, 0};
+
+		//store rotvel and update oldrot
+		float rot_vel=rot-old_rot;
+		old_rot=rot;
+
+		//verlet integration
+		float rot_acc=torques/moment_of_inertia;
+		rot+=rot_vel+rot_acc*dt*dt;
+		updateRot();
+
+		//reset torques
+		torques=0;
+	}
+#pragma endregion
 };
 
 void Shape::copyFrom(const Shape& s) {
@@ -130,12 +262,5 @@ void Shape::copyFrom(const Shape& s) {
 
 void Shape::clear() {
 	delete[] points;
-}
-
-//ensure shape is wound consistently
-void Shape::ensureWinding() {
-	if(getArea()<0) {
-		std::reverse(points, points+num_pts);
-	}
 }
 #endif//SHAPE_CLASS_H
