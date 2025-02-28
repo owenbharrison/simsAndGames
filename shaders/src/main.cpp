d1632361bb416a9f8287dfdec22acf3c7c7f0953
Owen Harrison <owenbharrison@gmail.com>
2025-02-28 15:42:44 -0700

    added rainbow_sobel, updated common includes

--- shaders/src/main.cpp
+++ shaders/src/main.cpp
@@ -2,7 +2,7 @@
 #define OLC_PGE_APPLICATION
 #include "common/olcPixelGameEngine.h"
 
-#include "shade_shader.h"
+#include "common/shade_shader.h"
 
 struct Demo : public olc::PixelGameEngine {
 	Demo() {
@@ -18,7 +18,42 @@ struct Demo : public olc::PixelGameEngine {
 	olc::Decal* target_dec=nullptr;
 
 	bool OnUserCreate() override {
-		effect=shade.MakeEffect(olc::fx::FX_SOBEL);
+		olc::EffectConfig rainbow_sobel{
+			DEFAULT_VS,
+			//x&y sobel kernel
+			"const int sobel[9]=int[9](-1,0,1,-2,0,2,-1,0,1);\n"
+			//three phase shifted sine waves
+			"const float Pi=3.1415927;\n"
+			"vec3 colorWheel(float angle) {\n"
+			"  return .5+.5*cos(angle+Pi*vec3(0,.667,-.667));\n"
+			"}\n"
+			"void main() {\n"
+			"  vec2 sz=1./vec2(textureSize(tex1, 0));\n"
+			"  vec2 sum=vec2(0);\n"
+					//accumulate convolution
+			"  for(int i=0; i<3; i++) {\n"
+			"    for(int j=0; j<3; j++) {\n"
+			"      vec2 offset_uv=sz*vec2(i, j);\n"
+			"      vec4 col=texture(tex1, xUV1+offset_uv);\n"
+							//luminance formula
+			"      float lum=dot(col.rgb, vec3(.299, .587, .114));\n"
+			"      sum+=lum*vec2(sobel[i+3*j], sobel[j+3*i]);\n"
+			"    }\n"
+			"  }\n"
+			"  float mag=length(sum);\n"
+					//which angle is edge?
+			"  float angle=atan(sum.y, sum.x);\n"
+			"  pix_out=vec4(mag*colorWheel(angle), 1);\n"
+			"}\n",
+			1,
+			1,
+		};
+		effect=shade.MakeEffect(rainbow_sobel);
+		if(!effect.IsOK()) {
+			std::cout<<effect.GetStatus();
+
+			return false;
+		}
 
 		source_spr=new olc::Sprite("assets/lenna.png");
 		source_dec=new olc::Decal(source_spr);
@@ -39,13 +74,16 @@ struct Demo : public olc::PixelGameEngine {
 
 	bool OnUserUpdate(float dt) override {
 		shade.Start(&effect);
+		//set ins/outs
 		shade.SetSourceDecal(source_dec);
 		shade.SetTargetDecal(target_dec);
+		//render?
 		shade.DrawDecal({0, 0}, source_dec);
 		shade.End();
 
 		Clear(olc::GREY);
 
+		//show final render.
 		DrawDecal({0, 0}, target_dec);
 
 		return true;
@@ -54,7 +92,7 @@ struct Demo : public olc::PixelGameEngine {
 
 int main() {
 	Demo d;
-	bool vsync=true;
+	bool vsync=false;
 	if(d.Construct(400, 400, 1, 1, false, vsync)) d.Start();
 
 	return 0;
