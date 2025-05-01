dca32edd8c45fba586ada5a510cb3a1410bee4f8
Owen Harrison <owenbharrison@gmail.com>
2025-04-30 23:57:08 -0600

    wall edit testing

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -1,461 +1,208 @@
 #define OLC_PGE_APPLICATION
 #include "common/olcPixelGameEngine.h"
 using olc::vf2d;
-using olc::vi2d;
 
 #include "common/utils.h"
 #include "common/aabb.h"
 namespace cmn {
 	using AABB=AABB_generic<vf2d>;
-
-	vf2d polar(float rad, float angle) {
-		return polar_generic<vf2d>(rad, angle);
-	}
-
-	vf2d lineLineIntersection(
-		const vf2d& a, const vf2d& b,
-		const vf2d& c, const vf2d& d) {
-		return lineLineIntersection_generic(a, b, c, d);
-	}
 }
 
 #include "common/stopwatch.h"
 
-//https://www.youtube.com/watch?v=7WcmyxyFO7o
-std::list<vf2d> poissonDiscSampling(const cmn::AABB& box, float rad) {
-	//determine spacing
-	float cell_size=rad/std::sqrtf(2);
-	int w=1+(box.max.x-box.min.x)/cell_size;
-	int h=1+(box.max.y-box.min.y)/cell_size;
-	vf2d** grid=new vf2d*[w*h];
-	for(int i=0; i<w*h; i++) grid[i]=nullptr;
-
-	//where can i spawn from?
-	std::list<vf2d> spawn_pts{box.getCenter()};
-
-	//as long as there are spawnable pts,
-	std::list<vf2d> pts;
-	while(spawn_pts.size()) {
-		//choose random spawn pt
-		auto it=spawn_pts.begin();
-		std::advance(it, rand()%spawn_pts.size());
-		const auto& spawn=*it;
-
-		//try n times to add pt
-		int k=0;
-		const int samples=20;
-		for(; k<samples; k++) {
-			float angle=cmn::random(2*cmn::Pi);
-			float dist=cmn::random(rad, 2*rad);
-			vf2d cand=spawn+cmn::polar(dist, angle);
-			if(!box.contains(cand)) continue;
-
-			//check 3x3 region around candidate
-			bool valid=true;
-			int ci=(cand.x-box.min.x)/cell_size;
-			int cj=(cand.y-box.min.y)/cell_size;
-			int si=std::max(0, ci-2);
-			int sj=std::max(0, cj-2);
-			int ei=std::min(w-1, ci+2);
-			int ej=std::min(h-1, cj+2);
-			for(int i=si; i<=ei; i++) {
-				for(int j=sj; j<=ej; j++) {
-					//if there is a point, and its too close,
-					const auto& idx=grid[i+w*j];
-					if(idx&&(*idx-cand).mag2()<rad*rad) {
-						valid=false;
-						break;
-					}
-				}
-				if(!valid) break;
-			}
-
-			//if no points too close, add the sucker
-			if(valid) {
-				if(ci<0||cj<0||ci>=w||cj>=h) continue;
-				pts.push_back(cand);
-				grid[ci+w*cj]=&pts.back();
-				spawn_pts.push_back(cand);
-				break;
-			}
-		}
-
-		//not spawnable enough, remove
-		if(k==samples) spawn_pts.erase(it);
-	}
-
-	delete[] grid;
-
-	return pts;
-}
-
-#include <set>
-
-//i dont want to pollute the global with triangle.
-namespace delaunay {
-	struct Triangle {
-		int p[3]{0, 0, 0};
-
-		Triangle() {}
-
-		Triangle(int a, int b, int c) {
-			if(a>b) std::swap(a, b);
-			if(b>c) std::swap(b, c);
-			if(a>b) std::swap(a, b);
-			p[0]=a, p[1]=b, p[2]=c;
-		}
-
-		bool contains(int a) const {
-			for(int i=0; i<3; i++) {
-				if(p[i]==a) return true;
-			}
-			return false;
-		}
-
-		bool equals(const Triangle& t) const {
-			for(int i=0; i<3; i++) {
-				if(t.p[i]!=p[i]) return false;
-			}
-			return true;
-		}
-
-		void getEdge(int e, int& p0, int& p1) const {
-			p0=p[e], p1=p[(e+1)%3];
-		}
-
-		static bool edgeShared(
-			const Triangle& t0, const int& e0,
-			const Triangle& t1, const int& e1
-		) {
-			int v00=0, v01=0, v10=0, v11=0;
-			t0.getEdge(e0, v00, v01);
-			t1.getEdge(e1, v10, v11);
-			if(v00==v10&&v01==v11) return true;
-			if(v00==v11&&v01==v10) return true;
-			return false;
-		}
-	};
-
-	struct Edge {
-		int p[2]{0, 0};
-
-		Edge() {}
-
-		Edge(int a, int b) {
-			if(a>b) std::swap(a, b);
-			p[0]=a, p[1]=b;
-		}
-
-		bool operator<(const Edge& e) const {
-			if(p[0]==e.p[0]) return p[1]<e.p[1];
-			return p[0]<e.p[0];
-		}
-	};
-
-	//https://en.wikipedia.org/wiki/Bowyer-Watson_algorithm
-	std::list<Triangle> triangulate(const std::vector<vf2d>& pts_ref) {
-		std::list<Triangle> tris;
-		std::vector<vf2d> pts=pts_ref;
-		int st_a=pts.size();
-		{//make super triangle
-			cmn::AABB box;
-			for(const auto& p:pts_ref) {
-				box.fitToEnclose(p);
-			}
-			box.min-=1, box.max+=1;
-			float x=box.min.x, y=box.min.y;
-			float w=box.max.x-x, h=box.max.y-y;
-			pts.emplace_back(x-h, y);
-			pts.emplace_back(x+w/2, y+h+w/2);
-			pts.emplace_back(x+w+h, y);
-			tris.emplace_back(st_a, st_a+1, st_a+2);
-		}
-
-		//perpendicular bisector
-		auto getCircumcircle=[] (const vf2d& a, const vf2d& b, const vf2d& c, vf2d& p, float& r_sq) {
-			float D=2*(a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y));
-			if(std::fabsf(D)<1e-6f) return false;
-
-			float a_sq=a.x*a.x+a.y*a.y;
-			float b_sq=b.x*b.x+b.y*b.y;
-			float c_sq=c.x*c.x+c.y*c.y;
-
-			p.x=(a_sq*(b.y-c.y)+b_sq*(c.y-a.y)+c_sq*(a.y-b.y))/D;
-			p.y=(a_sq*(c.x-b.x)+b_sq*(a.x-c.x)+c_sq*(b.x-a.x))/D;
-			r_sq=(p-a).mag2();
-
-			return true;
-		};
-
-		//add all the points one at a time to the triangulation
-		for(int p=0; p<st_a; p++) {
-			const auto& pt=pts[p];
-
-			//first find all the triangles that are no longer valid due to the insertion
-			std::list<Triangle> bad_tris;
-			for(const auto& t:tris) {
-				//dont check tri pts
-				if(t.contains(p)) continue;
-
-				vf2d pos;
-				float rad_sq;
-				bool cc=getCircumcircle(pts[t.p[0]], pts[t.p[1]], pts[t.p[2]], pos, rad_sq);
-				if(cc&&(pos-pt).mag2()<rad_sq) bad_tris.push_back(t);
-			}
-
-			//find the boundary of the polygonal hole
-			std::list<Edge> polygon;
-			for(const auto& bt:bad_tris) {
-				//for each edge in tri
-				for(int i=0; i<3; i++) {
-					//if edge NOT shared by any other tri in bad_tris
-					bool shared=false;
-					for(const auto& o_bt:bad_tris) {
-						if(&o_bt==&bt) continue;
-						for(int j=0; j<3; j++) {
-							if(Triangle::edgeShared(bt, i, o_bt, j)) {
-								shared=true;
-								break;
-							}
-						}
-						if(shared) break;
-					}
-					//add edge to polygon
-					if(!shared) {
-						int a, b;
-						bt.getEdge(i, a, b);
-						polygon.emplace_back(a, b);
-					}
-				}
-
-				//remove them from the data structure
-				for(auto it=tris.begin(); it!=tris.end();) {
-					if(it->equals(bt)) {
-						it=tris.erase(it);
-					} else it++;
-				}
-			}
-
-			//re-triangulate the polygonal hole
-			for(const auto& e:polygon) {
-				//form a triangle from edge to point
-				tris.emplace_back(e.p[0], e.p[1], p);
-			}
-		}
-
-		//done inserting points, now clean up
-		for(auto it=tris.begin(); it!=tris.end();) {
-			//if tri has super tri vert, remove
-			bool contains=false;
-			for(int i=0; i<3; i++) {
-				for(int j=0; j<3; j++) {
-					if(it->p[i]==st_a+j) {
-						contains=true;
-						break;
-					}
-				}
-				if(contains) break;
-			}
-			if(contains) {
-				it=tris.erase(it);
-			} else it++;
-		}
-
-		return tris;
-	}
-
-	//for connecting things up.
-	template<typename TriContainer>
-	std::set<Edge> extractEdges(const TriContainer& tris) {
-		std::set<Edge> edges;
-		for(const auto& t:tris) {
-			for(int i=0; i<3; i++) {
-				int a, b;
-				t.getEdge(i, a, b);
-				edges.emplace(a, b);
-			}
-		}
-		return edges;
-	}
-}
+struct Triangle {
+	vf2d a, b, c;
+};
 
 struct Example : olc::PixelGameEngine {
 	Example() {
-		sAppName="meshing testing";
+		sAppName="wall edit testing";
 	}
 
-	float rad=10;
+	bool editing=false;
+	bool edits[4], prev_edits[4];
+	int prev_mouse=-1;
+	int ix(int i, int j) const {
+		return i+2*j;
+	}
 
-	std::vector<vf2d> outer;
+	cmn::AABB edit_box;
+	vf2d edit_box_start;
 
-	int num_pts=0;
-	vf2d* points=nullptr;
-	std::list<delaunay::Edge> constraints, springs;
+	cmn::AABB bounds[4];
+
+	std::list<Triangle> triangles;
+
+	void recalculateBounds() {
+		static const float margin=10;
+		vf2d wh=(edit_box.max-edit_box.min-3*margin)/2;
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				vf2d ij(i, j);
+				vf2d xy=edit_box.min+margin*(1+ij)+wh*ij;
+				bounds[ix(i, j)]={xy, xy+wh};
+			}
+		}
+	}
 
 	bool OnUserCreate() override {
+		for(int i=0; i<4; i++) {
+			edits[i]=true;
+			prev_mouse=-1;
+		}
+
+		edit_box={{0, 0}, vf2d(ScreenWidth(), ScreenHeight())};
+
+		recalculateBounds();
+
 		return true;
 	}
 
 	bool OnUserUpdate(float dt) override {
 		const vf2d mouse_pos=GetMousePos();
 
-		//every now and then, add addition point
-		const auto add_action=GetMouse(olc::Mouse::LEFT);
-		if(add_action.bPressed) {
-			outer.clear();
-
-			delete[] points;
-			points=nullptr;
-			constraints.clear();
-			springs.clear();
+		if(GetMouse(olc::Mouse::MIDDLE).bPressed) {
+			edit_box_start=mouse_pos;
 		}
-		if(add_action.bHeld) {
-			//make sure points are far enough away
-			bool unique=true;
-			for(const auto& o:outer) {
-				if((o-mouse_pos).mag()<rad) {
-					unique=false;
-					break;
-				}
-			}
-			if(unique) outer.push_back(mouse_pos);
+		if(GetMouse(olc::Mouse::MIDDLE).bReleased) {
+			edit_box=cmn::AABB();
+			edit_box.fitToEnclose(edit_box_start);
+			edit_box.fitToEnclose(mouse_pos);
+			recalculateBounds();
 		}
-		if(add_action.bReleased) {
-			//distribute inside points
-			cmn::AABB box;
-			for(const auto& s:outer) {
-				box.fitToEnclose(s);
-			}
-			std::list<vf2d> inside=poissonDiscSampling(box, rad);
 
-			//remove outside points
-			for(auto it=inside.begin(); it!=inside.end(); ) {
-				vf2d dir=cmn::polar(1, cmn::random(2*cmn::Pi));
-
-				int num_ix=0;
-				for(int i=0; i<outer.size(); i++) {
-					const auto& a=outer[i], & b=outer[(i+1)%outer.size()];
-					vf2d tu=cmn::lineLineIntersection(a, b, *it, *it+dir);
-					if(tu.x>=0&&tu.x<=1&&tu.y>=0) num_ix++;
-				}
-
-				if(num_ix%2) it++;
-				else it=inside.erase(it);
+		if(editing) {
+			if(GetMouse(olc::Mouse::RIGHT).bPressed) {
+				for(int i=0; i<4; i++) edits[i]=true;
 			}
 
-			//allocate
-			num_pts=outer.size()+inside.size();
-			points=new vf2d[num_pts];
-			{//copy points over
-				int i=0;
-				for(const auto& p:outer) {
-					points[i++]=p;
-				}
-				for(const auto& p:inside) {
-					points[i++]=p;
-				}
-			}
-
-			//connect constraints	
-			for(int i=0; i<outer.size(); i++) {
-				constraints.emplace_back(i, (i+1)%outer.size());
-			}
-
-			//ensure winding
-			float area=0;
-			for(const auto& c:constraints) {
-				const auto& a=points[c.p[0]], & b=points[c.p[1]];
-				area+=a.x*b.y-a.y*b.x;
-			}
-			if(area<0) {
-				area*=-1;
-				//reverse each constraint
-				for(auto& c:constraints) {
-					std::swap(c.p[0], c.p[1]);
-				}
-				//reverse constraints
-				std::reverse(constraints.begin(), constraints.end());
-			}
-
-			//delaunay springs
-			std::vector<vf2d> tri_pts(num_pts);
-			for(int i=0; i<num_pts; i++) {
-				tri_pts[i]=points[i];
-			}
-			auto tris=delaunay::triangulate(tri_pts);
-			auto edges=delaunay::extractEdges(tris);
-			for(const auto& e:edges) {
-				springs.emplace_back(e.p[0], e.p[1]);
-			}
-
-			for(auto it=springs.begin(); it!=springs.end();) {
-				//check if already constrained
-				bool constrained=false;
-				for(const auto& c:constraints) {
-					if((c.p[0]==it->p[0]&&c.p[1]==it->p[1])||
-						(c.p[0]==it->p[1]&&c.p[1]==it->p[0])) {
-						constrained=true;
+			if(GetMouse(olc::Mouse::LEFT).bHeld) {
+				//find position
+				int curr_mouse=-1;
+				for(int i=0; i<4; i++) {
+					if(bounds[i].contains(mouse_pos)) {
+						curr_mouse=i;
 						break;
 					}
 				}
-				if(constrained) {
-					it=springs.erase(it);
-					continue;
+				//flip solids along swipe
+				if(curr_mouse!=-1&&curr_mouse!=prev_mouse) edits[curr_mouse]^=true;
+				prev_mouse=curr_mouse;
+			}
+			if(GetMouse(olc::Mouse::LEFT).bReleased) prev_mouse=-1;
+		}
+
+		if(GetKey(olc::Key::SHIFT).bPressed) {
+			if(editing) {
+				//evaluate edit
+				int state=edits[0]+2*edits[1]+4*edits[2]+8*edits[3];
+				int type=0, rot=0;
+				//1=square, 2=bar, 3=L, 4=fill
+				switch(state) {
+					case 1: type=1; break;
+					case 2: type=1, rot=1; break;
+					case 8: type=1, rot=2; break;
+					case 4: type=1, rot=3; break;
+					case 3: type=2; break;
+					case 10: type=2, rot=1; break;
+					case 12: type=2, rot=2; break;
+					case 5: type=2, rot=3; break;
+					case 7: type=3; break;
+					case 11: type=3, rot=1; break;
+					case 14: type=3, rot=2; break;
+					case 13: type=3, rot=3; break;
+					case 15: type=4; break;
 				}
 
-				//check if spring "leaves" shape
-				//naive: is midpoint inside shape?
-				{
-					vf2d pt=(points[it->p[0]]+points[it->p[1]])/2;
-					vf2d dir=cmn::polar(1, cmn::random(2*cmn::Pi));
-
-					int num_ix=0;
-					for(int i=0; i<outer.size(); i++) {
-						const auto& a=outer[i], & b=outer[(i+1)%outer.size()];
-						vf2d tu=cmn::lineLineIntersection(a, b, pt, pt+dir);
-						if(tu.x>=0&&tu.x<=1&&tu.y>=0) num_ix++;
+				if(type==0) {
+					//invalid edit
+					for(int i=0; i<4; i++) edits[i]=prev_edits[i];
+				} else {
+					//calc vertex positions
+					float xl=edit_box.min.x, xr=edit_box.max.x, xm=(xl+xr)/2;
+					float yt=edit_box.min.y, yb=edit_box.max.y, ym=(yt+yb)/2;
+					vf2d v[9];
+					v[0]={xl, yt}, v[1]={xm, yt}, v[2]={xr, yt};
+					v[7]={xl, ym}, v[8]={xm, ym}, v[3]={xr, ym};
+					v[6]={xl, yb}, v[5]={xm, yb}, v[4]={xr, yb};
+					//rotate vertexes
+					for(int r=0; r<2*rot; r++) {
+						vf2d temp=v[0];
+						for(int i=0; i<8; i++) v[i]=v[i+1];
+						v[7]=temp;
 					}
-
-					if(num_ix%2==0) {
-						it=springs.erase(it);
-						continue;
+					//tesselate
+					switch(type) {
+						case 1:
+							triangles={
+								{v[0], v[1], v[7]},
+								{v[1], v[8], v[7]}
+							};
+							break;
+						case 2:
+							triangles={
+								{v[0], v[2], v[7]},
+								{v[2], v[3], v[7]}
+							};
+							break;
+						case 3:
+							triangles={
+								{v[0], v[2], v[6]},
+								{v[2], v[3], v[8]},
+								{v[8], v[5], v[6]}
+							};
+							break;
+						case 4:
+							triangles={
+								{v[0], v[2], v[6]},
+								{v[2], v[4], v[6]}
+							};
+							break;
 					}
 				}
-
-				it++;
+				prev_mouse=-1;
+			} else {
+				//store edits
+				for(int i=0; i<4; i++) prev_edits[i]=edits[i];
 			}
-
-			outer.clear();
+			editing^=true;
 		}
 
-		Clear(olc::GREY);
+		Clear(olc::BLACK);
+		const olc::Pixel light_grey(230, 230, 230);
 
-		//show outer
-		for(const auto& o:outer) {
-			FillCircle(o, 2, olc::RED);
+		//show boxes
+		if(editing) {
+			for(int i=0; i<2; i++) {
+				for(int j=0; j<2; j++) {
+					const auto& box=bounds[ix(i, j)];
+					if(edits[ix(i, j)]) {
+						FillRect(box.min, box.max-box.min, olc::Pixel(0, 100, 255));
+						DrawRect(box.min, box.max-box.min, olc::BLUE);
+					} else {
+						DrawRect(box.min, box.max-box.min, light_grey);
+					}
+					DrawString(box.getCenter()-vf2d(8, 8), std::to_string(ix(i, j)), olc::BLACK, 2);
+				}
+			}
+		} else {
+			for(const auto& t:triangles) {
+				FillTriangle(t.a, t.b, t.c, light_grey);
+			}
+			for(const auto& t:triangles) {
+				DrawTriangle(t.a, t.b, t.c, olc::GREY);
+			}
 		}
 
-		if(points) {
-			for(const auto& s:springs) {
-				DrawLine(points[s.p[0]], points[s.p[1]], olc::BLACK);
-			}
+		DrawRect(edit_box.min, edit_box.max-edit_box.min, olc::GREEN);
 
-			for(const auto& c:constraints) {
-				DrawLine(points[c.p[0]], points[c.p[1]], olc::WHITE);
-			}
-
-			for(int i=0; i<num_pts; i++) {
-				FillCircle(points[i], 2, olc::BLUE);
-			}
-		}
-		
 		return true;
 	}
 };
 
 int main() {
 	Example e;
-	if(e.Construct(640, 480, 1, 1, false, true)) e.Start();
+	if(e.Construct(200, 200, 2, 2, false, true)) e.Start();
 
 	return 0;
 }
