79faaebdbc3cd57e1ca48df6e31e73590a53c4e7
Owen Harrison <owenbharrison@gmail.com>
2025-06-01 00:04:21 -0600

    textured billboard testing

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -1,200 +1,131 @@
-#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
-using olc::vf2d;
+#include "common/3d/engine_3d.h"
+using cmn::vf3d;
 
-#include "common/utils.h"
-#include "common/aabb.h"
-namespace cmn {
-	using AABB=AABB_generic<vf2d>;
-}
+constexpr float Pi=3.1415927f;
 
-#include "common/stopwatch.h"
-
-struct Triangle {
-	vf2d a, b, c;
+struct Billboard {
+	int id;
+	vf3d pos;
+	float w=1, h=1;
 };
 
-struct Example : olc::PixelGameEngine {
+struct Example : cmn::Engine3D {
 	Example() {
-		sAppName="wall edit testing";
+		sAppName="billboard texturing";
 	}
 
-	bool editing=false;
-	bool edits[4], prev_edits[4];
-	int prev_mouse=-1;
-	int ix(int i, int j) const {
-		return i+2*j;
+	//camera positioning
+	float cam_yaw=-Pi/2;
+	float cam_pitch=0;
+
+	std::vector<olc::Sprite*> texture_atlas;
+	std::vector<Billboard> billboards;
+
+	bool user_create() override {
+		cam_pos={0, 0, 2.5f};
+		
+		texture_atlas.push_back(new olc::Sprite("assets/mario_8bit.png")); 
+		billboards.push_back({0, vf3d(-3, 0, 0)});
+		
+		texture_atlas.push_back(new olc::Sprite("assets/luigi_8bit.png"));
+		billboards.push_back({1, vf3d(-1, 0, 0)});
+		
+		texture_atlas.push_back(new olc::Sprite("assets/mario.png"));
+		billboards.push_back({2, vf3d(1, 0, 0)});
+		
+		texture_atlas.push_back(new olc::Sprite("assets/luigi.png"));
+		billboards.push_back({3, vf3d(3, 0, 0)});
+		
+		return true;
 	}
 
-	cmn::AABB edit_box;
-	vf2d edit_box_start;
-
-	cmn::AABB bounds[4];
-
-	std::list<Triangle> triangles;
-
-	void recalculateBounds() {
-		static const float margin=10;
-		vf2d wh=(edit_box.max-edit_box.min-3*margin)/2;
-		for(int i=0; i<2; i++) {
-			for(int j=0; j<2; j++) {
-				vf2d ij(i, j);
-				vf2d xy=edit_box.min+margin*(1+ij)+wh*ij;
-				bounds[ix(i, j)]={xy, xy+wh};
-			}
-		}
+	bool user_destroy() override {
+		for(auto& t:texture_atlas) delete t;
+		
+		return true;
 	}
 
-	bool OnUserCreate() override {
-		for(int i=0; i<4; i++) {
-			edits[i]=true;
-			prev_mouse=-1;
-		}
+	bool user_update(float dt) override {
+		//look up, down
+		if(GetKey(olc::Key::UP).bHeld) cam_pitch+=dt;
+		if(cam_pitch>Pi/2) cam_pitch=Pi/2-.001f;
+		if(GetKey(olc::Key::DOWN).bHeld) cam_pitch-=dt;
+		if(cam_pitch<-Pi/2) cam_pitch=.001f-Pi/2;
 
-		edit_box={{0, 0}, vf2d(ScreenWidth(), ScreenHeight())};
+		//look left, right
+		if(GetKey(olc::Key::LEFT).bHeld) cam_yaw-=dt;
+		if(GetKey(olc::Key::RIGHT).bHeld) cam_yaw+=dt;
 
-		recalculateBounds();
+		//polar to cartesian
+		cam_dir=vf3d(
+			std::cosf(cam_yaw)*std::cosf(cam_pitch),
+			std::sinf(cam_pitch),
+			std::sinf(cam_yaw)*std::cosf(cam_pitch)
+		);
+
+		//move up, down
+		if(GetKey(olc::Key::SPACE).bHeld) cam_pos.y+=4.f*dt;
+		if(GetKey(olc::Key::SHIFT).bHeld) cam_pos.y-=4.f*dt;
+
+		//move forward, backward
+		vf3d fb_dir(std::cosf(cam_yaw), 0, std::sinf(cam_yaw));
+		if(GetKey(olc::Key::W).bHeld) cam_pos+=5.f*dt*fb_dir;
+		if(GetKey(olc::Key::S).bHeld) cam_pos-=3.f*dt*fb_dir;
+
+		//move left, right
+		vf3d lr_dir(fb_dir.z, 0, -fb_dir.x);
+		if(GetKey(olc::Key::A).bHeld) cam_pos+=4.f*dt*lr_dir;
+		if(GetKey(olc::Key::D).bHeld) cam_pos-=4.f*dt*lr_dir;
+
+		//set light pos
+		if(GetKey(olc::Key::L).bHeld) light_pos=cam_pos;
 
 		return true;
 	}
 
-	bool OnUserUpdate(float dt) override {
-		const vf2d mouse_pos=GetMousePos();
+	bool user_geometry() override {
+		for(const auto& b:billboards) {
+			vf3d norm=(b.pos-cam_pos).norm();
+			vf3d up(0, 1, 0);
+			vf3d rgt=norm.cross(up).norm();
+			up=rgt.cross(norm);
 
-		if(GetMouse(olc::Mouse::MIDDLE).bPressed) {
-			edit_box_start=mouse_pos;
-		}
-		if(GetMouse(olc::Mouse::MIDDLE).bReleased) {
-			edit_box=cmn::AABB();
-			edit_box.fitToEnclose(edit_box_start);
-			edit_box.fitToEnclose(mouse_pos);
-			recalculateBounds();
-		}
+			//vertex positioning
+			vf3d tl=b.pos+b.w/2*rgt+b.h/2*up;
+			vf3d tr=b.pos+b.w/2*rgt-b.h/2*up;
+			vf3d bl=b.pos-b.w/2*rgt+b.h/2*up;
+			vf3d br=b.pos-b.w/2*rgt-b.h/2*up;
 
-		if(editing) {
-			if(GetMouse(olc::Mouse::RIGHT).bPressed) {
-				for(int i=0; i<4; i++) edits[i]=true;
-			}
+			//texture coords
+			cmn::v2d tl_t{0, 0};
+			cmn::v2d tr_t{0, 1};
+			cmn::v2d bl_t{1, 0};
+			cmn::v2d br_t{1, 1};
 
-			if(GetMouse(olc::Mouse::LEFT).bHeld) {
-				//find position
-				int curr_mouse=-1;
-				for(int i=0; i<4; i++) {
-					if(bounds[i].contains(mouse_pos)) {
-						curr_mouse=i;
-						break;
-					}
-				}
-				//flip solids along swipe
-				if(curr_mouse!=-1&&curr_mouse!=prev_mouse) edits[curr_mouse]^=true;
-				prev_mouse=curr_mouse;
-			}
-			if(GetMouse(olc::Mouse::LEFT).bReleased) prev_mouse=-1;
+			//tesselation
+			cmn::Triangle f1{tl, br, tr, tl_t, br_t, tr_t}; f1.id=b.id;
+			tris_to_project.push_back(f1);
+			cmn::Triangle f2{tl, bl, br, tl_t, bl_t, br_t}; f2.id=b.id;
+			tris_to_project.push_back(f2);
 		}
 
-		if(GetKey(olc::Key::SHIFT).bPressed) {
-			if(editing) {
-				//evaluate edit
-				int state=edits[0]+2*edits[1]+4*edits[2]+8*edits[3];
-				int type=0, rot=0;
-				//1=square, 2=bar, 3=L, 4=fill
-				switch(state) {
-					case 1: type=1; break;
-					case 2: type=1, rot=1; break;
-					case 8: type=1, rot=2; break;
-					case 4: type=1, rot=3; break;
-					case 3: type=2; break;
-					case 10: type=2, rot=1; break;
-					case 12: type=2, rot=2; break;
-					case 5: type=2, rot=3; break;
-					case 7: type=3; break;
-					case 11: type=3, rot=1; break;
-					case 14: type=3, rot=2; break;
-					case 13: type=3, rot=3; break;
-					case 15: type=4; break;
-				}
+		return true;
+	}
 
-				if(type==0) {
-					//invalid edit
-					for(int i=0; i<4; i++) edits[i]=prev_edits[i];
-				} else {
-					//calc vertex positions
-					float xl=edit_box.min.x, xr=edit_box.max.x, xm=(xl+xr)/2;
-					float yt=edit_box.min.y, yb=edit_box.max.y, ym=(yt+yb)/2;
-					vf2d v[9];
-					v[0]={xl, yt}, v[1]={xm, yt}, v[2]={xr, yt};
-					v[7]={xl, ym}, v[8]={xm, ym}, v[3]={xr, ym};
-					v[6]={xl, yb}, v[5]={xm, yb}, v[4]={xr, yb};
-					//rotate vertexes
-					for(int r=0; r<2*rot; r++) {
-						vf2d temp=v[0];
-						for(int i=0; i<8; i++) v[i]=v[i+1];
-						v[7]=temp;
-					}
-					//tesselate
-					switch(type) {
-						case 1:
-							triangles={
-								{v[0], v[1], v[7]},
-								{v[1], v[8], v[7]}
-							};
-							break;
-						case 2:
-							triangles={
-								{v[0], v[2], v[7]},
-								{v[2], v[3], v[7]}
-							};
-							break;
-						case 3:
-							triangles={
-								{v[0], v[2], v[6]},
-								{v[2], v[3], v[8]},
-								{v[8], v[5], v[6]}
-							};
-							break;
-						case 4:
-							triangles={
-								{v[0], v[2], v[6]},
-								{v[2], v[4], v[6]}
-							};
-							break;
-					}
-				}
-				prev_mouse=-1;
-			} else {
-				//store edits
-				for(int i=0; i<4; i++) prev_edits[i]=edits[i];
-			}
-			editing^=true;
-		}
-
+	bool user_render() override {
 		Clear(olc::BLACK);
-		const olc::Pixel light_grey(230, 230, 230);
 
-		//show boxes
-		if(editing) {
-			for(int i=0; i<2; i++) {
-				for(int j=0; j<2; j++) {
-					const auto& box=bounds[ix(i, j)];
-					if(edits[ix(i, j)]) {
-						FillRect(box.min, box.max-box.min, olc::Pixel(0, 100, 255));
-						DrawRect(box.min, box.max-box.min, olc::BLUE);
-					} else {
-						DrawRect(box.min, box.max-box.min, light_grey);
-					}
-					DrawString(box.getCenter()-vf2d(8, 8), std::to_string(ix(i, j)), olc::BLACK, 2);
-				}
-			}
-		} else {
-			for(const auto& t:triangles) {
-				FillTriangle(t.a, t.b, t.c, light_grey);
-			}
-			for(const auto& t:triangles) {
-				DrawTriangle(t.a, t.b, t.c, olc::GREY);
-			}
+		//render 3d stuff
+		resetBuffers();
+
+		for(const auto& t:tris_to_draw) {
+			FillTexturedDepthTriangle(
+				t.p[0].x, t.p[0].y, t.t[0].u, t.t[0].v, t.t[0].w,
+				t.p[1].x, t.p[1].y, t.t[1].u, t.t[1].v, t.t[1].w,
+				t.p[2].x, t.p[2].y, t.t[2].u, t.t[2].v, t.t[2].w,
+				texture_atlas[t.id], t.col, t.id
+			);
 		}
-
-		DrawRect(edit_box.min, edit_box.max-edit_box.min, olc::GREEN);
 
 		return true;
 	}
@@ -202,7 +133,7 @@ struct Example : olc::PixelGameEngine {
 
 int main() {
 	Example e;
-	if(e.Construct(200, 200, 2, 2, false, true)) e.Start();
+	if(e.Construct(640, 320, 1, 1, false, true)) e.Start();
 
 	return 0;
 }
