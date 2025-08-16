e05c50f927482bc05ca3dbda5e91cfa08541ec71
Owen Harrison <owenbharrison@gmail.com>
2025-08-16 00:11:22 -0600

    adding emscripten build tool

---
+++ emscripten_build/compile.bat
@@ -0,0 +1,45 @@
+:: don't echo each command
+@echo off
+
+:: get build dir(current)
+set "build=%CD%"
+
+:: goto emsdk dir
+cd /d C:\emsdk
+
+:: start emscripten environment
+call emsdk_env.bat
+
+:: goto simsAndGames dir
+cd /d "%build%\.."
+
+:: prompt for project to compile
+set /p project=Enter project name: 
+
+if exist "%project%" (
+	:: goto project dir
+	cd "%project%"
+) else (
+	echo invalid project
+	goto wait
+)
+
+:: make out dir
+set "out=%build%\%project%"
+if not exist "%out%" mkdir "%out%"
+
+if exist assets (
+	echo compiling with assets...
+	call em++ -std=c++17 -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_LIBPNG=1 -I ..\common src\main.cpp -o "%out%\pge.html" --preload-file assets
+) else (
+	echo compiling without assets...
+	call em++ -std=c++17 -O2 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -s USE_LIBPNG=1 -I ..\common src\main.cpp -o "%out%\pge.html"
+)
+
+:: copy index.html
+copy "%build%\index.html" "%out%"
+
+echo success
+
+:wait
+pause
