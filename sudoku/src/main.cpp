afa66b20c85356f1e3ad3bdc3e7d2d4ebe4fa19e
Owen Harrison <owenbharrison@gmail.com>
2025-09-17 01:07:24 -0600

    actual wordle/sudoku deletion

--- sudoku/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "sudoku_ui.h"
-
-int main() {
-	SudokuUI game;
-	if(game.Construct(400, 400, 1, 1, false, true)) game.Start();
-
-	return 0;
-}
--- wordle/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "wordle_ui.h"
-
-int main() {
-	WordleUI wg;
-	bool vsync=true;
-	if(wg.Construct(400, 500, 1, 1, false, vsync)) wg.Start();
-
-	return 0;
-}
