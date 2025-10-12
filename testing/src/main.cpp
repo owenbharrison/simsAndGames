c345254857839f0a78b41d2eace20e5a008214a8
Owen Harrison <owenbharrison@gmail.com>
2025-10-11 19:51:28 -0600

    ui layout testing

--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -2,209 +2,138 @@
 #include "olcPixelGameEngine.h"
 using olc::vf2d;
 
-#include <random>
+#include <string>
+#include <vector>
+
+void getStringSize(const std::string& str, int& w, int& h) {
+	w=0, h=0;
+	int ox=0, oy=0;
+	for(const auto& c:str) {
+		if(c==' ') ox++;
+		else if(c=='\n') ox=0, oy++;
+		else if(c>='!'&&c<='~') {
+			ox++;
+			w=std::max(w, ox);
+			h=std::max(h, 1+oy);
+		}
+	}
+};
+
+struct Menu {
+	std::string title;
+	std::vector<std::string> options;
+};
 
 #include "common/aabb.h"
 namespace cmn {
 	using AABB=AABB_generic<vf2d>;
 }
 
-#include "perlin_noise.h"
-
-struct Bubble {
-	vf2d pos, vel, acc;
-	olc::Pixel col=olc::WHITE;
-	bool to_collide=false;
-
-	void accelerate(const vf2d& a) {
-		acc+=a;
-	}
-
-	void update(float dt) {
-		vel+=acc*dt;
-		pos+=vel*dt;
-		acc*=0;
-	}
-
-	void keepIn(const cmn::AABB& box) {
-		if(pos.x<box.min.x) {
-			pos.x=box.min.x;
-			vel.x*=-1;
-		}
-		if(pos.y<box.min.y) {
-			pos.y=box.min.y;
-			vel.y*=-1;
-		}
-		if(pos.x>box.max.x) {
-			pos.x=box.max.x;
-			vel.x*=-1;
-		}
-		if(pos.y>box.max.y) {
-			pos.y=box.max.y;
-			vel.y*=-1;
-		}
-	}
-};
-
-constexpr float Pi=3.1415927f;
-
-static float random01() {
-	static thread_local std::mt19937 rng{std::random_device{}()};
-	static thread_local std::uniform_real_distribution<float> dist(0, 1);
-	return dist(rng);
-}
-
 class Example : public olc::PixelGameEngine {
-	olc::Sprite* bubble_spr=nullptr;
-	olc::Decal* bubble_dec=nullptr;
+	int margin=50;
+	float scl_title=1.2f;
 
-	int max_bubbles=0;
-	float bubble_timer=0;
+	Menu main, diff;
 
-	const float bubble_rad=50;
-	std::vector<Bubble> bubbles;
-
-	cmn::AABB bounds;
-
-	PerlinNoise noise_gen;
-
-	float total_dt=0;
-
-	vf2d gravity;
+	vf2d pt_a, pt_b;
 
 public:
 	Example() {
-		sAppName="bubble screensaver";
+		sAppName="ui maths";
 	}
 
 public:
 	bool OnUserCreate() override {
-		bubble_spr=new olc::Sprite("assets/bubble.png");
+		main={"Main", {
+			"Start",
+			"Difficulty",
+			"Tutorial",
+			"Settings",
+			"Quit"
+		}};
 
-		//fix bubble sprite...
-		for(int i=0; i<bubble_spr->width; i++) {
-			for(int j=0; j<bubble_spr->height; j++) {
-				const auto& p=bubble_spr->GetPixel(i, j);
-
-				//alpha=luminance, col=white
-				int lum=(p.r+p.g+p.b)/3;
-				bubble_spr->SetPixel(i, j, olc::Pixel(255, 255, 255, lum));
-			}
-		}
-
-		bubble_dec=new olc::Decal(bubble_spr);
-
-		{//estimate bubble count
-			//circle packing efficiency
-			float usable_area=.907f*ScreenWidth()*ScreenHeight();
-
-			//use 50-75% of the screen
-			float said_pct=.5f+.25f*random01();
-			float said_area=said_pct*usable_area;
-
-			//how many bubbles can fit?
-			float bubble_area=Pi*bubble_rad*bubble_rad;
-			max_bubbles=said_area/bubble_area;
-		}
-
-		bounds={vf2d(0, 0), vf2d(ScreenWidth(), ScreenHeight())};
-		
-		noise_gen=PerlinNoise(std::time(0));
+		diff={"Difficulty", {
+			"Beginner",
+			"Normal",
+			"Hard"
+			"Expert",
+			"back"
+		}};
 
 		return true;
 	}
 
-	bool OnUserDestroy() override {
-		delete bubble_dec;
-		delete bubble_spr;
+	void renderMenu(const Menu& m, const cmn::AABB& box) {
+		const vf2d sz_box=box.max-box.min;
+		
+		int w_title=0, h_title=0;
+		getStringSize(m.title, w_title, h_title);
 
-		return true;
+		float char_h=scl_title*h_title;
+		for(const auto& o:m.options) {
+			int w=0, h=0;
+			getStringSize(o, w, h);
+			char_h+=h;
+		}
+		float scl_y=(sz_box.y-3*margin)/(8*char_h);
+
+		float char_w=scl_title*w_title;
+		for(const auto& o:m.options) {
+			int w=0, h=0;
+			getStringSize(o, w, h);
+			char_w=std::max(char_w, float(w));
+		}
+		float scl_x=(sz_box.x-2*margin)/(8*char_w);
+
+		float scl=std::min(scl_x, scl_y);
+
+		float cx=(box.min.x+box.max.x)/2;
+
+		float y=box.min.y+margin;
+		DrawStringDecal(
+			vf2d(cx-4*w_title*scl_title*scl,
+			y),
+			m.title,
+			olc::WHITE,
+			scl_title*scl*vf2d(1, 1)
+		);
+		y+=8*h_title*scl_title*scl;
+
+		y+=margin;
+
+		for(int i=0; i<m.options.size(); i++) {
+			const auto& o=m.options[i];
+			int w=0, h=0;
+			getStringSize(o, w, h);
+			DrawStringDecal(
+				vf2d(cx-4*w*scl,
+				y),
+				o,
+				olc::WHITE,
+				scl*vf2d(1, 1)
+			);
+			y+=8*h*scl;
+		}
 	}
 
 	bool OnUserUpdate(float dt) override {
-		if(GetKey(olc::Key::ESCAPE).bHeld) return false;
-		
-		if(bubbles.size()<max_bubbles) {
-			//spawn a new bubble every now and then
-			if(bubble_timer<0) {
-				bubble_timer+=.25f+.75f*random01();
+		const vf2d mouse_pos=GetMousePos();
 
-				//bottom left, random vel up and right
-				vf2d pos(0, ScreenHeight());
-				float speed=70+130*random01();
-				float angle=Pi*(1.5f+.5f*random01());
-				vf2d vel(speed*std::cos(angle), speed*std::sin(angle));
-				Bubble b_temp{pos, vel};
-				
-				//random bright color
-				int r=256*random01();
-				int g=256*random01();
-				int b=256*random01();
-				float l=std::max(r, std::max(g, b));
-				b_temp.col=olc::PixelF(r/l, g/l, b/l);
-				bubbles.push_back(b_temp);
-			}
-			bubble_timer-=dt;
-		}
+		if(GetKey(olc::Key::M).bHeld) margin=mouse_pos.y;
+		if(GetKey(olc::Key::A).bHeld) pt_a=mouse_pos;
+		if(GetKey(olc::Key::B).bHeld) pt_b=mouse_pos;
 
-		//update gravity
-		gravity.x=100-200*noise_gen.noise(.25f*total_dt, 0);
-		gravity.y=100-200*noise_gen.noise(0, .25f*total_dt);
+		cmn::AABB box;
+		box.fitToEnclose(pt_a);
+		box.fitToEnclose(pt_b);
 
-		//physics
-		for(auto& b:bubbles) {
-			b.keepIn(bounds);
+		renderMenu(main, box);
 
-			//first find collisions
-			std::vector<Bubble*> collisions;
-			for(auto& o:bubbles) {
-				//dont check self
-				if(&o==&b) continue;
+		//draw box
+		DrawRectDecal(box.min, box.max-box.min, olc::RED);
 
-				//dont check non-colliding
-				if(!o.to_collide) continue;
-
-				//broad phase detection
-				vf2d sub=o.pos-b.pos;
-				if(sub.mag2()<4*bubble_rad*bubble_rad) {
-					collisions.push_back(&o);
-				}
-			}
-			if(b.to_collide) {
-				//solve collisions
-				for(auto& c:collisions) {
-					//push bubbles apart
-					vf2d sub=b.pos-c->pos;
-					float dist_sq=sub.mag2();
-					float dist=std::sqrt(dist_sq);
-					float diff=dist-2*bubble_rad;
-					vf2d norm=sub/dist;
-					vf2d delta=.5f*diff*norm;
-					b.pos-=delta, c->pos+=delta;
-
-					//inelastic collision
-					//special case where m1=m2
-					float v_rel=norm.dot(b.vel-c->vel);
-					const float restitution=.5f;
-					float impulse=-(1+restitution)*v_rel/2;
-					vf2d vel_delta=impulse*norm;
-					b.vel+=vel_delta, c->vel-=vel_delta;
-				}
-			}
-			//start colliding if not colliding
-			else if(collisions.empty()) b.to_collide=true;
-
-			//kinematics
-			b.accelerate(gravity);
-			b.update(dt);
-		}
-
-		for(const auto& b:bubbles) {
-			vf2d scl=2*bubble_rad/vf2d(bubble_spr->width, bubble_spr->height);
-			DrawDecal(b.pos-bubble_rad, bubble_dec, scl, b.col);
-		}
-
-		total_dt+=dt;
+		//draw margin box
+		DrawRectDecal(box.min+margin, box.max-box.min-2*margin, olc::GREEN);
 
 		return true;
 	}
@@ -212,9 +141,9 @@ public:
 
 int main() {
 	Example demo;
-	bool fullscreen=true;
+	bool fullscreen=false;
 	bool vsync=true;
-	if(demo.Construct(1920, 1080, 1, 1, fullscreen, vsync)) demo.Start();
+	if(demo.Construct(800, 600, 1, 1, fullscreen, vsync)) demo.Start();
 
 	return 0;
 }
