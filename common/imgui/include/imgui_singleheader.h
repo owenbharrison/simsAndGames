9e3c7c150ca7f5617d6af6989d20799452b31194
Owen Harrison <owenbharrison@gmail.com>
2026-03-10 00:37:14 -0600

    common: added imgui headers

---
+++ common/imgui/include/imgui_singleheader.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef IMGUI_SINGLEHEADER_H
+#define IMGUI_SINGLEHEADER_H
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui.h"
+#include "imgui.cpp"
+#include "imgui_demo.cpp"
+#include "imgui_draw.cpp"
+#include "imgui_tables.cpp"
+#include "imgui_widgets.cpp"
+
+#endif
